62120c5576bf207fb8146304f5331413e0ca2eff
Marcos <marcosnovo@gmail.com>
2026-04-10 12:50:48 +0200

    Initial Commit

---
+++ geometry/geometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  geometryApp.swift
+//  geometry
+//
+//  Created by Marcos on 10/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct geometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ geometryTests/geometryTests.swift
@@ -0,0 +1,19 @@
+//
+//  geometryTests.swift
+//  geometryTests
+//
+//  Created by Marcos on 10/04/2026.
+//
+
+import Testing
+@testable import geometry
+
+struct geometryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
